a2e2022b7ac3050aa89f003e6d7ee3f9e0a35bf0
Web-44 <xdk63778x@gmail.com>
2019-08-23 19:02:30 +0200

    Added missing code

--- src/main/java/de/web/labymod/acaddon/lmac/Parser.java
+++ src/main/java/de/web/labymod/acaddon/lmac/Parser.java
@@ -1,1 +1,44 @@
+package de.web.labymod.acaddon.lmac;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.JsonObject;
+
+import de.web.labymod.acaddon.Main;
+
+public class Parser {
+	
+	private static Map<String, LmacAction> actions = new HashMap<String, LmacAction>();
+	
+	public static void init() {
+		register(new ActionCommunity());
+		register(new ActionFriends());
+		register(new ActionGame());
+		register(new ActionHideRank());
+		register(new ActionNick());
+		register(new ActionParty());
+		register(new ActionRank());
+	}
+	
+	private static void register(LmacAction action) {
+		actions.put(action.type, action);
+	}
+	
+	public static void onMessage(JsonObject obj) {
+		if (!obj.has("type") || !obj.has("data")) return;
+		String type = obj.get("type").getAsString();
+		if (actions.containsKey(type)) {
+			if (obj.get("data").isJsonObject()) {
+				actions.get(type).execute(obj.get("data").getAsJsonObject());
+			} else {
+				Main.instance.getApi().displayMessageInChat("§8[§bLaby§2AC§8] §cDetected an invalid built message: " + type);
+				Main.instance.getApi().displayMessageInChat("§8[§bLaby§2AC§8] §cPlease report this bug on §ats.AntiCraft.de");
+			}
+		} else {
+			Main.instance.getApi().displayMessageInChat("§8[§bLaby§2AC§8] §cDetected an invalid message type: " + type);
+			Main.instance.getApi().displayMessageInChat("§8[§bLaby§2AC§8] §cPlease report this bug on §ats.AntiCraft.de");
+		}
+	}
+	
+}
